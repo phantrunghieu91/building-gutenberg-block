d914f5779d2b151227984f1067dd47374ff60e1d
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-06-06 15:13:40 +0700

    UPDATE: global style - Created a global css file - Registered global css file

--- wordpress/wp-content/themes/jin-dev/functions.php
+++ wordpress/wp-content/themes/jin-dev/functions.php
@@ -1,5 +1,9 @@
 <?php 
 
 require_once __DIR__ . '/inc/RegisterBlocks.php';
-
 new RegisterBlocks();
+
+function theme_styles() {
+  wp_enqueue_style( 'global', get_template_directory_uri() . '/assets/css/global.css', [], null, 'all');
+}
+add_action('wp_enqueue_scripts', 'theme_styles');
