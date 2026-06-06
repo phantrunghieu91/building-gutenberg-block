b7e4eaafac8232f56d8539ce701a0321359697ae
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-06-07 00:56:26 +0700

    UPDATE: style - Moved layer level declare to wp_head hook

--- wordpress/wp-content/themes/jin-dev/functions.php
+++ wordpress/wp-content/themes/jin-dev/functions.php
@@ -2,8 +2,10 @@
 
 require_once __DIR__ . '/inc/RegisterBlocks.php';
 new RegisterBlocks();
-
+add_action( 'wp_head', function() {
+  echo '<style id="jins-layers-level">@layer reset, base, utilities, components;</style>';
+}, 5 );
 function theme_styles() {
   wp_enqueue_style( 'global', get_template_directory_uri() . '/assets/css/global.css', [], null, 'all');
 }
-add_action('wp_enqueue_scripts', 'theme_styles');
+add_action('wp_enqueue_scripts', 'theme_styles', 1);
