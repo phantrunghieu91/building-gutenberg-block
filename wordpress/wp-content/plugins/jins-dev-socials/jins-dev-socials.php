c17bab504b7118529deb55cd24bd770e3acf9436
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-06-15 12:44:28 +0700

    UPDATE: jins-dev-socials - Check display on website option, if active, render a block to mount socials block later on

--- wordpress/wp-content/plugins/jins-dev-socials/jins-dev-socials.php
+++ wordpress/wp-content/plugins/jins-dev-socials/jins-dev-socials.php
@@ -5,7 +5,7 @@
  * Version:           0.1.0
  * Requires at least: 6.8
  * Requires PHP:      7.4
- * Author:            The WordPress Contributors
+ * Author:            Hieu "Jin" Phan Trung
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       jins-dev-socials
@@ -16,23 +16,33 @@
 if ( ! defined( 'ABSPATH' ) ) {
   exit; // Exit if accessed directly.
 }
-define( 'PLUGIN_JINS_DEV_SOCIALS_PATH', plugin_dir_path(__FILE__));
-define( 'PLUGIN_JINS_DEV_SOCIALS_URI', plugin_dir_url(__FILE__));
+define( 'PLUGIN_JINS_DEV_SOCIALS_PATH', plugin_dir_path( __FILE__ ) );
+define( 'PLUGIN_JINS_DEV_SOCIALS_URI', plugin_dir_url( __FILE__ ) );
 
 // Register option page for setup socials globally
 require_once __DIR__ . '/inc/register-options-pages.php';
 $option_page_instance = new JinsRegisteringOptionsPages();
 
-function jins_dev_jins_dev_socials_block_init() {
+function jins_dev_socials_block_init() {
   wp_register_block_types_from_metadata_collection( __DIR__ . '/build', __DIR__ . '/build/blocks-manifest.php' );
 }
+function jins_dev_socials_display_on_website() {
+  $settings = get_option( 'jins_dev_socials_fe_display_settings' );
+  if( isset( $settings['will_display_on_web'] ) && true === $settings['will_display_on_web'] ) {
+    add_action( 'wp_footer', function() {
+      echo '<div id="jins-dev-socials-root"></div>';
+    } );
+  }
+}
+add_action( 'init', 'jins_dev_socials_display_on_website' );
 function plugin_activate() {
-  add_action( 'init', 'jins_dev_jins_dev_socials_block_init' );
+  add_action( 'init', 'jins_dev_socials_block_init' );
   flush_rewrite_rules();
 }
 register_activation_hook( __FILE__, 'plugin_activate' );
 
 function plugin_deactivate() {
+	remove_action('init', 'jins_dev_socials_display_on_website');
   flush_rewrite_rules();
 }
 register_deactivation_hook( __FILE__, 'plugin_deactivate' );
