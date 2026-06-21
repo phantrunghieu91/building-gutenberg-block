d10dc45a6ccdd94ed87ff0f18bdbb45865c9b963
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-06-21 11:55:03 +0700

    FIXED: webpack config - Make sure it still compile block code

--- wordpress/wp-content/plugins/jins-dev-socials/webpack.config.js
+++ wordpress/wp-content/plugins/jins-dev-socials/webpack.config.js
@@ -1,10 +1,14 @@
 const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 const path = require('path');
 
-module.exports = {
-  ...defaultConfig,
-  entry: {
-    ...defaultConfig.entry, // ← keep existing block entries
-    'jins-dev-socials-general-settings': path.resolve(__dirname, 'src/jins-dev-socials-general-settings.js'),
-  },
+module.exports = async ( env ) => {
+  const entry = await defaultConfig.entry();
+
+  return {
+    ...defaultConfig,
+    entry: {
+      ...entry,
+      'jins-dev-socials-general-settings': path.resolve( __dirname, 'src/jins-dev-socials-general-settings.js' ),
+    },
+  };
 };
