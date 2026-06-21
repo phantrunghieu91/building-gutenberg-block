aaa1fced2b3e2418340a278cabaa3b57dbb6e366
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-06-21 11:56:13 +0700

    UPDATE: Block script, add SVG icon

--- wordpress/wp-content/plugins/jins-dev-socials/src/jins-dev-socials/index.js
+++ wordpress/wp-content/plugins/jins-dev-socials/src/jins-dev-socials/index.js
@@ -20,14 +20,12 @@ import './style.scss';
 import Edit from './edit';
 import metadata from './block.json';
 
-/**
- * Every block starts by registering a new block type definition.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
- */
+const svgIcon = (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M18.5 5.5V8H20V5.5h2.5V4H20V1.5h-1.5V4H16v1.5h2.5zM12 4H6a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2v-6h-1.5v6a.5.5 0 01-.5.5H6a.5.5 0 01-.5-.5V6a.5.5 0 01.5-.5h6V4z"></path></svg>);
+
 registerBlockType( metadata.name, {
 	/**
 	 * @see ./edit.js
 	 */
+	icon: svgIcon,
 	edit: Edit,
 } );
